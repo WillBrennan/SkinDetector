230abd61d413015fce0610ae92909fe176440a85
WillBrennan <william.brennan@kcl.ac.uk>
2015-03-15 15:49:16 +0000

    Added main_script

---
+++ main_script.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__author__ = 'Will Brennan'
+
+
+# Built-in Modules
+import logging
+# Standard Modules
+import cv2
+import numpy
+import cPickle
+# Custom Modules
+
+
+if __name__ == '__main__':
+    pass
