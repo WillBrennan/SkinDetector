dae6382532b8ac3aea70e08cd504f3abe38d92d9
WillBrennan <WillBrennan@users.noreply.github.com>
2017-01-18 12:04:59 +0000

    added outline of unit tests

--- skin_detector/__init__.py
+++ skin_detector/__init__.py
@@ -2,7 +2,9 @@
 # -*- coding: utf-8 -*-
 __author__ = 'willbrennan'
 
-from skin_detector import process
+from skin_detector import *
 from scripts import display
 
-__all__ = ["process", "display"]
+__all__ = [
+    "process", "display", "get_hsv_mask", "get_rgb_mask", "get_ycrcb_mask", "grab_cut_mask", "grab_cut_mask", "closing"
+]
--- tests/skin_detector/test_unit.py
+++ tests/skin_detector/test_unit.py
@@ -1,28 +1,35 @@
 import cv2
 import numpy
+import skin_detector
 
 
 def test_get_hsv_mask():
     img = cv2.imread("../test_image.png")
-    assert True
+    mask = skin_detector.get_hsv_mask(img)
+    assert img.shape[:2] == mask.shape
 
 
 def test_get_rgb_mask():
     img = cv2.imread("../test_image.png")
-    assert True
+    mask = skin_detector.get_rgb_mask(img)
+    assert img.shape[:2] == mask.shape
 
 
 def test_get_ycrcb_mask():
     img = cv2.imread("../test_image.png")
-    assert True
+    mask = skin_detector.get_ycrcb_mask(img)
+    assert img.shape[:2] == mask.shape
+
 
 def test_grab_cut_mask():
     img = cv2.imread("../test_image.png")
     assert True
 
+
 def test_closing():
     img = cv2.imread("../test_image.png")
     assert True
+
 
 def test_process():
     img = cv2.imread("../test_image.png")
