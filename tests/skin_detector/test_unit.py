eb7c58fee3e85ef90b3f3ae613b175f7b5c05d60
WillBrennan <WillBrennan@users.noreply.github.com>
2017-01-18 11:53:55 +0000

    added outline of tests

--- tests/skin_detector/test_unit.py
+++ tests/skin_detector/test_unit.py
@@ -1,10 +1,29 @@
-import unittest
+import cv2
+import numpy
 
 
-class BasicTestSuite(unittest.TestCase):
-    def test_dummy(self):
-        assert True
+def test_get_hsv_mask():
+    img = cv2.imread("../test_image.png")
+    assert True
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_get_rgb_mask():
+    img = cv2.imread("../test_image.png")
+    assert True
+
+
+def test_get_ycrcb_mask():
+    img = cv2.imread("../test_image.png")
+    assert True
+
+def test_grab_cut_mask():
+    img = cv2.imread("../test_image.png")
+    assert True
+
+def test_closing():
+    img = cv2.imread("../test_image.png")
+    assert True
+
+def test_process():
+    img = cv2.imread("../test_image.png")
+    assert True
